7ac83c505b96e5cc87e5449859f83a5332d434b2
ishida <h_hand_3636@gmail.com>
2020-10-11 12:35:35 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = (env,argv) => {
+    return{
+        //モードの設定
+        mode: "production",
+        //エントリーポイントの設定
+        entry: {
+            index:path.join(__dirname,"index.ts"),
+        },
+        //出力先の設定
+        output:{
+            path: path.join(__dirname,"www"),
+            filename:"test_npm.js",
+            library:"test_npm",
+            libraryTarget:"umd"
+        },
+        module:{
+            //JavaScriptファイル以外の他の言語で書かれたプログラムをwebpackで扱う場合にはLoader(ローダー)を使用
+            rules:[
+                {
+                    test:/\.ts$/,
+                    use:[
+                        {
+                            loader:"ts-loader"
+                        }
+                    ]
+                }
+            ]
+        },
+        //dev-serverの設定
+        devServer:{
+            contentBase:"www",
+            port:8080
+        },
+        performance: { hints: false }
+    }
+}
